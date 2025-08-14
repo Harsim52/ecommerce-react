a615baa16a7b8ff85832f718ce7e25b6017475f9
Harsim52 <Harsim52@gmail.com>
2025-08-14 15:39:03 +0530

    base change

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react(), ],
-  // base: '/ecommerce-react/',
-    base: '/',
+  base: '/ecommerce-react/',
+    // base: '/',
 
 })
